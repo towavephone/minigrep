8125904d11ad57b0132be1efd16049762c413287
tubt <tubt@xiaopeng.com>
2023-10-09 11:36:51 +0800

    init commit

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+use std::env;
+use std::process;
+
+use minigrep::run;
+use minigrep::Config;
+
+fn main() {
+    // dbg!(&args);
+
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+
+    if let Err(e) = run(config) {
+        eprintln!("Application error: {e}");
+        process::exit(1);
+    }
+}
